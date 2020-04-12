77fbf52e05c517b3ee282ac3dc39144ddc2a1908
siddharthsinghjd <39839817+siddharthsinghjd@users.noreply.github.com>
2020-04-12 15:46:21 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,14 +1,7 @@
-var http = require('http'),
-    fs = require('fs');
-
-
-fs.readFile('./index.html', function (err, html) {
-    if (err) {
-        throw err; 
-    }       
-    http.createServer(function(request, response) {  
-        response.writeHeader(200, {"Content-Type": "text/html"});  
-        response.write(html);  
-        response.end();  
-    }).listen(8000);
+var http = require('http');
+var port = process.env.PORT || 8080;        
+var server = http.createServer(function(req, res) {
+    res.writeHead(200);
+    res.end('Hello Heroku This are the new changes!!!');
 });
+server.listen(port);
